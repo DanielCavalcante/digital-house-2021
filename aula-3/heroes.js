9c4f3a786862ab5207ca43dfad385e497f1f5759
Daniel Cavalcante <danielcdesouza@gmail.com>
2021-11-03 23:56:43 -0300

    Add lesson 3

---
+++ aula-3/heroes.js
@@ -0,0 +1,19 @@
+const heroes = [
+    {
+        nome: "Batman",
+        rico: true,
+        acessorios: ["Cinto", "Capa", "batrangue", "Luvas"]    
+    },
+    {
+        nome: "Chapolin Colorado",
+        rico: false,
+        acessorios: ["Antenas", "Marreta Biônica", "Buzina Paralisadoura"]
+    },
+    {
+        nome: "Mulher Maravilha",
+        rico: true,
+        acessorios: ["Corda da verdade", "Cinto", "Espada", "Braceleted"]
+    }
+]
+
+module.exports = heroes
