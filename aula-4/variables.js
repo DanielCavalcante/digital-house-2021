e7754c7c05354cc9169da263eeeb8857d2b293ff
Daniel Cavalcante <danielcdesouza@gmail.com>
2021-11-10 21:52:50 -0300

    add lesson 4

---
+++ aula-4/variables.js
@@ -0,0 +1,10 @@
+var nome = 'Xuxinha'; //Não utilizar mais var
+
+let nomeCompleto = 'Xuxinha de Jesus';
+console.log(nomeCompleto)
+nomeCompleto = 'Xuxinha Santos'
+console.log(nomeCompleto)
+
+const digitalHouseVariavel = 'Digital House';
+console.log(digitalHouseVariavel)
+//digitalHouseVariavel = 'Digital House Web Full-Stack'
